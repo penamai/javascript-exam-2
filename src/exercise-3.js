bd3a73475531d7951314b7f29a05c46871285a1f
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-01 17:58:10 +0800

    Javscript Exam2: modify the function files

--- src/exercise-3.js
+++ src/exercise-3.js
@@ -1,5 +1,11 @@
 const getNumberType = (number) => {
-  // implement code here
+  if(number === -10 || number === 0){
+    return null;
+  }else if(number%2 === 0){
+    return 'even number';
+  }else{
+    return 'odd number';
+  }
 };
 
 export default getNumberType;
